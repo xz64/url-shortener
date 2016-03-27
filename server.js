d6d9bec82bab692d4f81767c9e9aae31430b406a
S K <xz64@users.noreply.github.com>
2016-03-26 21:10:40 -0400

    add mongodb connection

--- server.js
+++ server.js
@@ -1,7 +1,40 @@
 var express = require('express');
+var MongoClient = require('mongodb').MongoClient;
 
 var port = +process.env.PORT || 8080;
+var mongoURI = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/urls';
+var db;
+var collection;
 var app = express();
+
+function connectMongo() {
+  return new Promise(function(resolve, reject) {
+    MongoClient.connect(mongoURI, function(err, _db) {
+      if(err) {
+        reject(err);
+      }
+      else {
+        db = _db;
+        resolve();
+      }
+    });
+  });
+}
+
+function setupMongo() {
+  return new Promise(function(resolve, reject) {
+    db.createCollection('urls', function(err, _collection) {
+      if(err) {
+        reject(err);
+      }
+      else {
+        db = _db;
+        collection = _collection;
+        resolve();
+      }
+    });
+  });
+}
 
 app.get('/new/:url', function(req, res) {
   res.end(req.params.url);
@@ -11,4 +44,11 @@ app.get('/:id(\\d+)', function(req, res) {
   res.end(req.params.id);
 });
 
-app.listen(port);
+connectMongo()
+.then(setupMongo())
+.then(function() {
+  app.listen(port);
+})
+.catch(function(err) {
+  console.log(err);
+});
