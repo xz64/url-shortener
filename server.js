14f24dd07bad8ad3a5ec270138f72d689d3cd355
S K <xz64@users.noreply.github.com>
2016-03-26 20:09:40 -0400

    add server request handlers

--- server.js
+++ server.js
@@ -3,4 +3,12 @@ var express = require('express');
 var port = +process.env.PORT || 8080;
 var app = express();
 
+app.get('/new/:url', function(req, res) {
+  res.end(req.params.url);
+});
+
+app.get('/:id(\\d+)', function(req, res) {
+  res.end(req.params.id);
+});
+
 app.listen(port);
